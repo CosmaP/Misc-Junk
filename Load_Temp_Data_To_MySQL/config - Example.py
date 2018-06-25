944c0e016e9b4d0164ac3060c1d9a315f78cf7a2
CosmaP <cosma@papouis.net>
2018-06-25 18:42:16 +0100

    Example Config file

---
+++ Load_Temp_Data_To_MySQL/config - Example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# SQL Setup
+HOST = '192.168.xx.xx'
+UNIX_SOCKET = '/tmp/mysql.sock'
+USER = 'root'
+PASSWD = 'xxxxxxxxxxxxx'
+DB = 'tempdata'
+
+AUT0COMMIT = True
+
+#======================================================================	
+# __main__ Code
+#======================================================================	   
+    
+if __name__ == "__main__":
+    print("\n\nThis file cannot be run directly. It is intended to be imported\n\n")
+else:
+    print("\n\nImporting config.py")
+    
+# End of __main__ Code
+#======================================================================
