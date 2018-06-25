e124ca346601b915ba0c431e94f758ad16e886de
Cosma Papouis <cosma@papouis.net>
2018-06-25 18:38:52 +0100

    Example Config File

---
+++ Picture_Resize_and_Upload/config - example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Size - (Width, Height)
+SIZE = (2000, 300)
+
+# Folders
+INFOLDER = 'PicturesIn'
+OUTFOLDER = 'PicturesOut'
+THUMBNAILS = 'PicturesOut/thumbnails'
+REJECTSFOLDER = 'Rejects'
+LOGFOLDER = 'Logs'
+
+# Web Storage Credentials
+FTPSERVER = 'Address'
+FTPPORT = '21'
+FTPFOLDER = 'images'
+FTPUSERNAME = 'FTPUsername'
+FTPPASSWORD = 'FTPpassword'
+
+
+#======================================================================	
+# __main__ Code
+#======================================================================	   
+    
+if __name__ == "__main__":
+    print("\n\nThis file cannot be run directly. It is intended to be imported\n\n")
+else:
+    print("\n\n................... Importing config.py ................")
+    
+# End of __main__ Code
+#======================================================================
