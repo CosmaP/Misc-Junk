2fde7b25065a3595f2b99e89b1b9dc172a33b03e
Cosma Papouis <cosma@papouis.net>
2023-05-04 18:58:07 +0100

    Working through training

--- Python_Pandas/main.py
+++ Python_Pandas/main.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python#
+#!/usr/bin/env python
 
 #======================================================================
 # Set-up 
 
+#ptpython
 import pandas as pd
+import csv
+import os
 
 def setup():                 # Shutdown GPIO and Cleanup modules
 
     print ('\n\nSetting Up ...\n')
+    # Print the current working directory
+    print("Current working directory: {0}".format(os.getcwd()))
+    # Change the current working directory
+    #os.chdir('Python_Pandas')
+    # Print the current working directory
+    #print("Current working directory: {0}".format(os.getcwd()))
+
 
         
 # End of set-up Procedures  
@@ -31,6 +41,44 @@ def maincontrol():                  # Main Control Loop
 
     print ('\n\nMain Loop ...\n')
 
+    csvfile = open('Lottery_Powerball_Winning_Numbers__Beginning_2010.csv')
+    
+    LotteryReader = csv.reader(csvfile)
+
+    # Read File 1 time
+    for row in LotteryReader:
+        print(row)
+    print('*****************************************************************************************************************')
+    print('*****************************************************************************************************************')
+
+    # Return position in file back to the begging
+    csvfile.seek(0)
+
+    data = list(LotteryReader)
+
+    print(data)
+
+    header = data[0]
+    data = data[1:]
+
+    print(header)
+
+    for i, row in enumerate(data):
+        date, numbers, number = row
+        if number == '':
+            number = 1
+        else:
+            number = int(number)
+        data[i] = [date, numbers, number]
+    
+    print(data)
+
+    with open('Lottery_Powerball_Modified.csv', 'w') as csvfile2:
+        LotteryWriter = csv.writer(csvfile2)  
+        LotteryWriter.writerow(header)
+        LotteryWriter.writerows(data)
+
+    
 # Main functionality goes here
 
 # End of Main Control Procedure        
