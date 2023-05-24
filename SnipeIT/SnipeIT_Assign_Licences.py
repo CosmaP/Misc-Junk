9fbdd1eda30bcf6a6d4011b02a3103b999422a88
Cosma Papouis <cosma@papouis.net>
2023-05-24 09:35:29 +0100

    modified:   SnipeIT/SnipeIT_Assign_Licences.py

--- SnipeIT/SnipeIT_Assign_Licences.py
+++ SnipeIT/SnipeIT_Assign_Licences.py
@@ -3,11 +3,12 @@
 ###########################################################################################
 #
 # Note:     This assigns licenses to existing users in Snipe
-#           The Input file is comes from an export from Snipe-IT and must be in this format
+#           The Input file is comes from an export from CodeTwo and must be in this format
+#           Username capitalisation must match Snipe IT usernames 
 #
-# Name,Title,Email,Phone,username,Department,Location,Manager,Assets,License,Consumables,Accessories,Notes,Groups,Login Enabled
-# Fred Bloggs,,,,Fred.Bloggs,,,,0,0,0,0,,,
-# Bert Brown,,,,Bert.Brown,,,,1,10,0,0,,Admin 
+# number,username
+# 1,Fred.Bloggs
+# 2,Bert.Brown
 #
 ###########################################################################################
 
@@ -17,6 +18,7 @@ import csv
 import json
 from datetime import datetime
 import logging
+import time
 
 # Set some parameters for this run
 api_url = config.API_URL
@@ -24,6 +26,7 @@ api_token = config.API_TOKEN
 input_file = config.INPUT_FILE
 output_file = config.OUTPUT_FILE
 license_id = config.LICENSE_ID
+initial_seat = config.INITIAL_SEAT
 
 ###################### Set up Logging
 logfile = config.LOGFILE
@@ -36,7 +39,7 @@ logfile = logfile + modified + ".log"
 
 # configure logging
 FORMAT = '%(asctime)-20s %(levelname)-8s %(message)s'
-logging.basicConfig(filename=logfile, level=logging.DEBUG, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename=logfile, level=logging.INFO, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
 
 ###################### End of Logging Setup
 
@@ -64,7 +67,7 @@ def getusernames(input_file):
             usersthatrequirelicenses.append(user_data)
       
     datafile.close()
-    #print(usersthatrequirelicenses)
+    
     return usersthatrequirelicenses
 
 def getuserids(api_url, headers, output_file):
@@ -73,7 +76,7 @@ def getuserids(api_url, headers, output_file):
     print("\n\n................... Getting ID and Username ................")
     userdetails = []
         
-    response = requests.get(f'{api_url}/users?limit=200&offset=0&sort=created_at&order=desc&deleted=false&all=false"', headers=headers)
+    response = requests.get(f'{api_url}/users?limit=200&offset=0&sort=created_at&order=desc&deleted=false&all=false"', headers=headers, verify=False)
     data = json.loads(response.content)  # Convert the response content to a dictionary
     fieldnames = ['id', 'username'] 
                 
@@ -99,46 +102,81 @@ def addlicenses(api_url, headers, license_id, licensesrequired, userdetails ):
     logging.info('Adding Licenses')
     print("\n\n................... Adding Licenses ................\n\n")
     count = 0
+    seat = initial_seat - 1
+    runcount = 0
+    wait2 = 0 
+    failures = []
+ 
     for username1 in licensesrequired:
-        print(username1)
+        index = 0
+        # print(username1)
+        length = len(userdetails)
+        # print(length)
         for username2 in userdetails:
-            user1 = username1['username']
-            user2 = username2['username']
+            index = (index + 1) % length  # Move to the next index (wrapping around if necessary)
+            #print(index)
+            if index == 0:
+                logging.error('%s is not a user in Snipe IT.' % username1)
+                print('%s is not a user in Snipe IT.' % username1)
+                failures.append('%s is not a user in Snipe IT.' % username1)
+                count = count - 1
+                break
+            user1 = username1['username'].lower()
+            user2 = username2['username'].lower()
             if user1 == user2:
-                print(count, username1, username2)
+                # print(count, username1, username2)
                 count += 1
+                seat += 1
                 
                 currentid = username2['id']
                 payload = {"assigned_to": currentid}
-                response = requests.put(f'{api_url}/licenses/{license_id}/seats/{count}', headers=headers, json=payload)
-                logging.info(f'Adding Licenses {count}: {user2}')
-                print(count, username2)
+                logging.info(f'Adding License {count}: Seat {seat}:  {username2}')
+                print(f'Adding License {count}: Seat {seat}:  {username2}')
+                response = requests.put(f'{api_url}/licenses/{license_id}/seats/{seat}', headers=headers, verify=False, json=payload)
+                runcount += 1
         
-    # Check the response status code
-    # if response.status_code == 200 it has been successful:
-    if response.status_code == 200:
-        logging.info(f'{count} User licenses added successfully!')
-        print('\n\n' + str(count) + ' User licenses added successfully!')
-    else:
-        logging.error(f'Error adding User licenses: {response.content}')
-        print('\n\nError adding User licenses:', response.content)
+                # Check the response status code
+                # if response.status_code == 200 it has been successful:
+                if response.status_code == 200:
+                    logging.info(f'{count} User licenses added successfully!')
+                    print('\n\n' + str(count) + ' User licenses added successfully!')
+                else:
+                    logging.error(f'Error adding User license {seat}: {response.content}')
+                    print('\n\nError adding User license {seat}:', response.content)
+                   
+                if runcount > 99:
+                    runcount = 0
+                    wait1 = 0
+                    wait2 = wait2 + 1
+                    while wait1 < 46:
+                        print('Pausing for 45 seconds: ', wait1, '- Loop (this is typically 1): ', wait2)
+                        time.sleep(1)
+                        wait1 = wait1 + 1
+                    
+                break   
+ 
+    logging.error('Users that failed:' )   
+    print(f'\n\nUsers that failed:\n')
+    for failure in failures:       
+        logging.error(failure)     
+        print({failure})        
     return response.status_code
-    
+                
     
 def main():
         
-    #try:
-    headers = setupheader(api_token)
-    userdetails = getuserids(api_url, headers, output_file)
-    licensesrequired = getusernames(input_file)
-    addlicenses(api_url, headers, license_id, licensesrequired, userdetails )
-    
-    logging.info('End of Run')
-    print("\n\n................... End of Run ................\n\n")
-    #except:
-    #    logging.error('Create groups failed')
-    #    print("\n\n................... Create groups failed! ................\n\n")
-    #    exit(1)
+    try:
+        headers = setupheader(api_token)
+        userdetails = getuserids(api_url, headers, output_file)
+        licensesrequired = getusernames(input_file)
+        addlicenses(api_url, headers, license_id, licensesrequired, userdetails )
+        
+        logging.info('End of Run')
+        print("\n\n................... End of Run ................\n\n")
+    except:
+        logging.error('Adding user licenses had some errors. Please investigate!')
+        print("\n\n................... Adding user licenses had some errors. Please investigate ................\n\n")
+        exit(1)
     
    
     
@@ -148,11 +186,11 @@ def main():
 #======================================================================	   
     
 if __name__ == "__main__":
-    #try:
-    main()
-    #except:
-    logging.error('Create groups failed!')
-    print("\n\n................... Create groups failed! ................\n\n")
+    try:
+        main()
+    except:
+        logging.error('Adding user licenses had some errors. Please investigate')
+        print("\n\n................... Adding user licenses had some errors. Please investigate ................\n\n")
     
         # end main
 else:
