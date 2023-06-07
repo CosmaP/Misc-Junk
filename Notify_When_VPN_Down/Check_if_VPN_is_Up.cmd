cd8fd37ccd7f00a1baa3a632da520e8ee43e51dc
Cosma Papouis <cosma@papouis.net>
2023-06-07 13:26:19 +0100

    Modify Batch file to user V3

--- Notify_When_VPN_Down/Check_if_VPN_is_Up.cmd
+++ Notify_When_VPN_Down/Check_if_VPN_is_Up.cmd
@@ -1,5 +1,5 @@
 cd .\
 #powershell -executionpolicy bypass -command ".\Check_if_VPN_is_Up_V01_MessageBox.ps1"
-powershell -executionpolicy bypass -command ".\Check_if_VPN_is_Up_V02_Toast.ps1"
+powershell -executionpolicy bypass -command ".\Check_if_VPN_is_Up_V03_Toast.ps1"
 
 # -noexit
