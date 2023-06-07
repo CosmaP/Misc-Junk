adb89e0e8ad1c82846a98f261ea719474726dbb4
Cosma Papouis <cosma@papouis.net>
2023-06-07 15:40:24 +0100

    Clean up files and folders

--- Notify_When_VPN_Down/Check_if_VPN_is_Up.cmd
+++ Notify_When_VPN_Down/Check_if_VPN_is_Up.cmd
@@ -1,5 +1,3 @@
+@echo off
 cd .\
-#powershell -executionpolicy bypass -command ".\Check_if_VPN_is_Up_V01_MessageBox.ps1"
 powershell -executionpolicy bypass -command ".\Check_if_VPN_is_Up_V03_Toast.ps1"
-
-# -noexit
